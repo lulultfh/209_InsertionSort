3192ad8c49002e5dffac8e999e85ee5587be4ad2
lulultfh <punyalulu05@gmail.com>
2024-03-14 15:33:30 +0700

    add display

--- 209_insertionsort/209_insertionsort.cpp
+++ 209_insertionsort/209_insertionsort.cpp
@@ -45,6 +45,18 @@ void insertionSort() {
     }
 }
 
+void display() {
+	cout << endl;
+	cout << "==========" << endl;
+	cout << "elemen Array yang tersusun" << endl;
+	cout << "==========" << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << endl; //Output setiap elemen Array pada garis baru
+	}
+
+	cout << "jumlah pass = " << n - 1 << endl; //menampilkan total angka dari elemen yang bener
+	cout << endl;
+}
 
     
 int main()
